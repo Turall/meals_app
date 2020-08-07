f75028220408fd34db0a1500906c0d3acd69b9a3
Tural <tural_m@hotmail.com>
2020-08-07 21:00:44 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DeliMeals",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: _MyHomePage(),
+    );
+  }
+}
+
+class _MyHomePage extends StatefulWidget {
+  @override
+  __MyHomePageState createState() => __MyHomePageState();
+}
+
+class __MyHomePageState extends State<_MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DeliMeals"),
+      ),
+      body: Center(
+        child: Center(
+          child: Text("Navigation Time!!"),
+        ),
+      ),
+    );
+  }
+}
